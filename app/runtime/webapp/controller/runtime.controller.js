8c60a0100de7ba445243052090743e00675daa00
Mahesa Lizardy <88304455+lizardyy@users.noreply.github.com>
2025-06-05 14:49:26 +0700

    feat: template before refactor

--- app/runtime/webapp/controller/runtime.controller.js
+++ app/runtime/webapp/controller/runtime.controller.js
@@ -31,28 +31,67 @@ sap.ui.define([
 
         _bindContextNode: function (sContextNodeId) {
             // Get a reference to the books table control by its ID
-            const oTable = this.byId("ContextNodeTable");
+            const oForm = this.byId("ContextNodeForm");
+            const oOtherForm = this.byId("BotInstanceForm");
 
             // If no author ID is provided, unbind the table and exit
             if (!sContextNodeId) {
-                oTable.unbindItems();
+                oForm.setVisible(false);
+                oForm.unbindItems();
+                return;
+            } else {
+                oForm.setVisible(true);
+                oOtherForm.setVisible(false);
+                // Bind the table items to the /Books entity set, filtered by the selected author's ID
+                const sPath = "/ContextNodes(" + sContextNodeId + ")";
+
+                oForm.bindElement({
+                    path: sPath,
+                });
+            }
+        },
+
+        // This is Detail page
+        onBotInstanceSelect: function () {
+            // Get the reference to the author list control by its ID
+            const oList = this.byId("BotInstancesList");
+
+            // Get the currently selected item (author) from the list
+            const oBotInstanceSelected = oList.getSelectedItem();
+
+            // If no author is selected, exit the function
+            if (!oBotInstanceSelected) {
                 return;
             }
 
-            // Bind the table items to the /Books entity set, filtered by the selected author's ID
-            oTable.bindItems({
-                path: "/ContextNodes", // OData entity set
-                filters: [new Filter("ID", FilterOperator.EQ, sContextNodeId)], // Show only books matching the selected author
-                template: new sap.m.ColumnListItem({
-                    cells: [
-                        // Display the book title
-                        new sap.m.Text({ text: "{label}" }),
-                        // Display the book description
-                        new sap.m.Text({ text: "{type}" }),
-
-                    ],
-                }),
-            });
+            // Retrieve the ID of the selected author from its binding context
+            const sBotInstanceId = oBotInstanceSelected.getBindingContext().getProperty("ID");
+            console.log(sBotInstanceId)
+            // Call a private function to bind and display books related to the selected author
+            this._bindBotInstance(sBotInstanceId);
         },
+
+        _bindBotInstance: function (sBotInstanceId) {
+            // Get a reference to the books table control by its ID
+            const oForm = this.byId("BotInstanceForm");
+            const oOtherForm = this.byId("ContextNodeForm");
+
+            // If no author ID is provided, unbind the table and exit
+            if (!sBotInstanceId) {
+                oForm.setVisible(false);
+                oForm.unbindItems();
+                return;
+            } else {
+                oForm.setVisible(true);
+                oOtherForm.setVisible(false);
+                // Bind the table items to the /Books entity set, filtered by the selected author's ID
+                const sPath = "/BotInstances(" + sBotInstanceId + ")";
+
+                oForm.bindElement({
+                    path: sPath,
+                });
+            }
+        },
+
     });
 });
