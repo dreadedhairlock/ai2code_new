be21e3cd3e46f4a97ae047b466431ea306ea2792
clarice-silvanus <claricesilvanus@gmail.com>
2025-06-13 16:16:53 +0700

    feat: added service folder and java files

---
+++ srv/src/main/java/com/sap/cap/ai2code/service/ContextService.java
@@ -0,0 +1,21 @@
+package com.sap.cap.ai2code.service;
+
+import java.util.List;
+import java.util.Map;
+
+import cds.gen.mainservice.ContextNodes;
+
+public interface ContextService {
+    public List<Map<String, Object>> buildContextAsHierarchy(List<ContextNodes> contextNodes);
+
+    public String getContextFullPath(String botInstanceId, String subPath);
+
+    public ContextNodes upsertContext(
+            String botInstanceId,
+            String taskId,
+            // Integer sequence,
+            // String contextPath,
+            String contextValue);
+    public ContextNodes getContextNode(
+            String contextNodeId);
+}
